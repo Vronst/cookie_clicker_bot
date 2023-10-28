20256decaabf60c4ae3e938715ae5f7886c8dac5
Adam S <113335218+Vronst@users.noreply.github.com>
2023-10-28 21:45:05 +0200

    cleaned code
    cleaned code from unnecesarry comments

--- cookie_clicker.py
+++ cookie_clicker.py
@@ -13,7 +13,7 @@ driver = webdriver.Chrome(chrome_options)
 
 driver.get(url=url_cookie)
 
-# consent = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]/')
+
 consent = driver.find_element(By.CLASS_NAME, value='fc-button')
 consent.click()
 time.sleep(5)
@@ -21,27 +21,21 @@ language = driver.find_element(By.XPATH, '//*[@id="langSelect-EN"]')
 language.click()
 time.sleep(10)
 
-# print(cookies)=
+
 
 
 def made_cookies():
     cookie = driver.find_element(By.ID, value='bigCookie')
-    # cookies = int(driver.find_element(By.ID, value='cookies').text.split()[0])
-    # price0 = int(driver.find_element(By.ID, value='productPrice0').text)
-    # price1 = int(driver.find_element(By.ID, value='productPrice0').text)
-    # price2 = int(driver.find_element(By.ID, value='productPrice0').text)
-    # price3 = int(driver.find_element(By.ID, value='productPrice0').text)
-    # price4 = int(driver.find_element(By.ID, value='productPrice0').text)
-    # price = min(price4, price3, price2, price1, price0)
+    
     start = time.time()
     end = 0
     buyout = []
     for x in range(1):
         while end - start < 5:
-            # print(price, cookies)
+           
             end = time.time()
             cookie.click()
-        # money = int(driver.find_element(By.ID, value='cookies').text.split()[0].replace(',', ''))
+       
         affordable = driver.find_elements(By.CSS_SELECTOR, value='.unlocked ')
 
         try:
@@ -52,8 +46,7 @@ def made_cookies():
         except:
             pass
         money = int(driver.find_element(By.ID, value='cookies').text.split()[0].replace(',', ''))
-        # for _ in affordable:
-        #   buyout.append(int(_.text.split('\n')[1]))
+        
         while True:
             buyout = [int(_.text.split('\n')[1].replace(',', '')) for _ in affordable]
             looking_for = max(buyout)
@@ -64,17 +57,7 @@ def made_cookies():
             else:
                 break
 
-        # try:
-        #     print(upgrade[0].text)
-        #     upgrading = [int(_.text.split('\n')[1].replace(',', '')) for _ in upgrade]
-        #     looking_for_up = min(upgrading)
-        #     looking_for_up = upgrading.index(looking_for_up)
-        #     if min(buyout) < min(upgrading):
-        #         affordable[looking_for].click()
-        #     else:
-        # upgrade[looking_for_up].click()
-        #       except IndexError:
-        #     print('Error')
+        
         affordable[looking_for].click()
 
 
